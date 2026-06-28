2b8b513f69ffdd5959c61d6ddb2f526716100220
Rube-Cube <rube-cube@outlook.com>
2026-06-27 20:41:05 -0700

    Fix typo in constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 PRIMES = [2, 3, 5, 7, 11, 13, 17, 23]
 KEY_LENGTH_EXCEPTION = "key too long.\n           seriously,\n           what are you trying to encrypt?\n           The Library of Babel?"
-PLACEHOLDER_HASHING_MODULUS = 265
+PLACEHOLDER_HASHING_MODULUS = 256
